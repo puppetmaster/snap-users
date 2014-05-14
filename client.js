9c6ce702d5fe567bccf2a30e6d4898dbe4b6c8aa
Philippe Caseiro <pcaseiro@cadoles.com>
2014-05-14 11:06:18 +0200

    Make storage work

--- client.js
+++ client.js
@@ -1,1 +1,4 @@
+supervisor.server.call('user', 'add', 'user', 'toto', 'titi', function (err){
+        console.log(arguments)
+   })
 supervisor.bridge('users', 'add');
--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 //
 // UserManagement snap plugin
 //
+var crypto = require('crypto');
+var algo = 'sha512';
+var key = 'snap!|&;)45'
+var encoding = 'hex';
 
-function add(params, cb) {
+function cryptPassword(password, cb) {
+    var hmac = crypto.createHmac(algo, key);
+    hmac.setEncoding(encoding);
+    hmac.write(password);
+    hmac.end();
+    return hmac.read()
+}
+
+
+function User(store){
+    this._store = store;
+   }
+
+var pUser = User.prototype;
+
+pUser.add = function (params, cb) {
     var type = params[0];
     var name = params[1];
+    var pass = params[2];
 
     if (type === 'user'){
+      this._store.get(name, function (err, value){
+          if (err) {
+            if (err.notFound) {
+                // handle a 'NotFoundError' here
+                return
+            }
+            // I/O or other error, pass it up the callback chain
+            return callback(err)
+          }
+        });
+      user = {};
+      user['name'] = name;
+      user['password'] = cryptPassword(pass);
+      user['type'] = 'user';
       console.log('Adding User');
+      this._store.put(name, user, cb);
+    else {
+          return cb(new Error('User allready exists'))
+      }
     }
     else if (type === 'group') {
       console.log('Adding Group');
+      return cb()
     }
     else {
       return cb(new Error('Unknown type :' + type));
@@ -18,8 +57,13 @@ function add(params, cb) {
   }
 
 function usersLoad(opts, cb) {
+    var usersStore = this.storage.getPluginStore('snap-users');
+    console.log("Loading users module");
+
+    var user = new User(usersStore)
+
     // Expose methods to Supervisor over RPC
-    this.rpc.expose('dir', 'add', add.bind(this));
+    this.rpc.expose('user', 'add', user.add.bind(user));
 
     // Inject client.js to the client
     this.plugins.injectClientSide(__dirname + '/client.js' );
